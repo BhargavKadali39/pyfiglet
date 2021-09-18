f0ef6d3f7fbbf3054669c06465119536914f4982
Bhargav <bhargavkadali39@gmail.com>
2021-09-18 23:00:30 +0530

    Update pyfiglet_texts.py

--- pyfiglet_texts.py
+++ pyfiglet_texts.py
@@ -1,1 +1,15 @@
+import pyfiglet as pyf
 
+def figletty(){
+  pyf_text =  pyf.figlet_format("How are y'all")
+  # Converting string into figlet component
+  
+  print(pyf_text)
+  # Printing the figlet component
+
+}
+figletty()
+pfy_choice = str(input('Type yes if you want to try it out').lower())
+if(pfy_choice == 'yes' | 's'):
+  figletty()
+  
