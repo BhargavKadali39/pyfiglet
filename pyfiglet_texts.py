f16f9e4ea98f381a57cdb890b79720446869cf08
Bhargav <bhargavkadali39@gmail.com>
2021-09-18 23:02:10 +0530

    Update pyfiglet_texts.py
    added dynamic coding

--- pyfiglet_texts.py
+++ pyfiglet_texts.py
@@ -1,12 +1,18 @@
 import pyfiglet as pyf
 
 
+def figletty():
+    print(pyf_text)
 
 pyf_text = pyf.figlet_format("How are y'all")
-print(pyf_text)
-pyf_choice = str(input('Type yes if you want to try it\n').lower())
-if pyf_choice == 'yes':
-   pyf_text = pyf.figlet_format(input('Enter data :\n')
-else:
-     print('Arigatou')
-     break
+
+figletty()
+
+while True:
+    pyf_choice = str(input('Type yes if you want to try it\n').lower())
+    if pyf_choice == 'yes':
+        pyf_text = pyf.figlet_format(input('Enter data :\n'))
+        figletty()
+    else:
+        print('Arigatou')
+        break
